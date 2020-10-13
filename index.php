215d83bb23b308083d8365b93fbb00e2c920b991
p.chatchai <kmitl50ite15engineer@gmail.com>
2020-10-14 01:24:20 +0700

    ****

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+	$a = [
+		['id' => 111, 'name' => 'Chatchai', 'surname' => 'Plukchalee'],
+		['id' => 112, 'name' => 'Orratai', 'surname' => 'Plulchalee']
+	];
+	
+//	print_r($a) ;
+?>
+
+HHH

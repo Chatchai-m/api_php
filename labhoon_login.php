60c89d9131dceb5f7d0cc53ff1eda3fc879ce1b5
p.chatchai <kmitl50ite15engineer@gmail.com>
2020-11-10 12:44:17 +0700

    ****

---
+++ labhoon_login.php
@@ -0,0 +1,24 @@
+<?php
+header("content-type:text/javascript;charset=utf-8");
+error_reporting(0);
+error_reporting(E_ERROR | E_PARSE);
+$link = mysqli_connect('localhost', 'food', '990110?', "food");
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    
+    exit;
+}
+
+if (!$link->set_charset("utf8"))
+{
+  printf("Error loading character set utf8: %s\n", $link->error);
+  exit();
+}
+
+echo "Hello world";
+
+mysqli_close($link);
+?>
